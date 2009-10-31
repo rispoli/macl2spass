a7d1f1cd12e26375604ae6a0c6d9dbdbddc11096
Daniele Rispoli <daniele.rispoli@gmail.com>
2009-10-31 12:34:50 +0100

    modified:   dsl.scm 		- s/interpretation/assignment/g 		- update-world gets called only by toplevel and leq and R are not 		  generated with useless data 		- various (list ...) replaced with `(,...)

--- dsl.scm
+++ dsl.scm
@@ -35,7 +35,7 @@
     ((:: (:+ (:or lower-letter upper-letter)) (:* (:or lower-letter upper-letter "_" digit))) (token-PROP_ATOM (string->symbol lexeme)))))
 
 (define dslp
-  (lambda (source-name interpretation initial-world)
+  (lambda (source-name assignment initial-world)
     (parser
       (src-pos)
       (start start)
@@ -61,54 +61,49 @@
                ((toplevel) $1))
         (expr ((simple-expr) $1)
               ((NOT expr) (let ((new-world (gensym "w")))
-                            `(forall ,new-world (implies
-                                                  (leq ,initial-world ,new-world)
-                                                  (not ,(update-world initial-world new-world interpretation $2))))))
+                            `(forall ,new-world (implies (leq) (not ,$2)))))
               ((expr AMPERAMPER expr) `(and ,$1 ,$3))
               ((expr BARBAR expr) `(or ,$1 ,$3))
               ((expr MINUSGREATER expr) (let ((new-world (gensym "w")))
-                                          `(forall ,new-world (implies
-                                                                (and (leq ,initial-world ,new-world) ,(update-world initial-world new-world interpretation $1))
-                                                                ,(update-world initial-world new-world interpretation $3)))))
+                                          `(forall ,new-world (implies (and (leq) ,$1) ,$3))))
               ((BOX LPAREN PROP_ATOM COMMA expr RPAREN) (let ((new-world (gensym "w")))
-                                                          `(forall ,new-world (implies
-                                                                                (R ,$3 ,initial-world ,new-world)
-                                                                                ,(update-world initial-world new-world interpretation $5))))))
+                                                          `(forall ,new-world (implies (R ,$3) ,$5)))))
         (expr-list ((expr-list DOT expr) (cons $3 $1))
                    ((expr) (list $1))
                    ((expr-list DOT) $1)
                    (() '()))
         (mode ((MODE) $1))
-        (toplevel ((LIST_OF_FORMULAE LPAREN mode RPAREN expr-list END_OF_LIST ) (list $3 (foldl cons '()
-                                                                                                (map (lambda (s) (list 'formula s)) $5)))))
-        (simple-expr ((PROP_ATOM) (list interpretation $1 initial-world))
+        (toplevel ((LIST_OF_FORMULAE LPAREN mode RPAREN expr-list END_OF_LIST ) `(,$3 ,(map (lambda (s)
+                                                                                              `(formula ,(update-world initial-world initial-world assignment s)))
+                                                                                            $5))))
+        (simple-expr ((PROP_ATOM) `(,assignment ,$1 ,initial-world))
                      ((BOOL) $1)
                      ((LPAREN expr RPAREN) $2))))))
 
 (define update-world
-  (lambda (initial-world new-world interpretation code)
+  (lambda (initial-world new-world assignment code)
     (letrec ((update-world-inner
                (lambda (code)
                  (cond
                    ((or (eqv? code 'true) (eqv? code 'false)) code)
-                   ((eqv? (car code) interpretation) (list interpretation (cadr code) new-world))
+                   ((eqv? (car code) assignment) `(,assignment ,(cadr code) ,new-world))
                    (else
                      (case (car code)
                        ((forall) (let ((initial-world new-world) (new-world (cadr code)))
-                                   `(forall ,new-world ,(update-world initial-world new-world interpretation (caddr code)))))
-                       ((leq) `(leq ,initial-world ,new-world)) ;; DO WE Really need to generate the information that we just replaced previously?? (Same for R)
+                                   `(forall ,new-world ,(update-world initial-world new-world assignment (caddr code)))))
+                       ((leq) `(leq ,initial-world ,new-world))
                        ((not) `(not ,(update-world-inner (cadr code))))
                        ((R) `(R ,(cadr code) ,initial-world ,new-world))
                        (else `(,(car code) ,(update-world-inner (cadr code)) ,(update-world-inner (caddr code))))))))))
       (update-world-inner code))))
 
 (define translate
-  (lambda (s interpretation initial-world #:src-name (src-name "current-input-port"))
+  (lambda (s assignment initial-world #:src-name (src-name "current-input-port"))
     (let ((ois (open-input-string s)) (statements '()))
       (port-count-lines! ois)
       (letrec ((loop
                  (lambda ()
-                   (let ((r ((dslp src-name interpretation initial-world) (lambda () (dsll ois)))))
+                   (let ((r ((dslp src-name assignment initial-world) (lambda () (dsll ois)))))
                      (when r
                        (set! statements (cons r statements))
                        (loop))))))
